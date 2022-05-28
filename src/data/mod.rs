9ebfe287f7f7c73d4f17a80d3bbfb3b52011f207
FL03 <jo3mccain@icloud.com>
2022-05-28 10:00:58 -0500

    Commit
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 use bson;
 
 pub mod structs;
 
+pub type Buffer = HashMap<String, String>;
 pub type Clock = bson::DateTime;
-pub type ObjectId = bson::oid::ObjectId;
-
+pub type Bid = bson::oid::ObjectId;
