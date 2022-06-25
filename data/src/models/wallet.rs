3085bf86da0c02f053780079dc93e8e73cf14b65
FL03 <jo3mccain@icloud.com>
2022-06-25 00:58:06 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- data/src/models/wallet.rs
+++ data/src/models/wallet.rs
@@ -5,3 +5,24 @@
     Description:
         ... Summary ...
  */
+use bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+
+pub type AccessGrant = [String; 12];
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Profile {
+    pub appellation: String,
+    pub data: Vec<crate::Container>,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Account {
+    pub access: AccessGrant,
+    pub data: [Profile; 3],
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Wallet {
+    pub accounts: Vec<Account>,
+}
