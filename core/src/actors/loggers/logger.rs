127990da3afec860a0162b0e0600a564ca4070c4
FL03 <jo3mccain@icloud.com>
2022-06-24 23:47:22 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- core/src/actors/loggers/logger.rs
+++ core/src/actors/loggers/logger.rs
@@ -1,14 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-pub trait LoggerSpec {
-    type Level;
-    type Settings;
-
-    fn setup(&self, settings: Self::Settings) -> Self;
-}
-
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct Logger {
     pub level: String,
 }
 
+impl Logger {
+    pub fn constructor(configuration: Box<dyn crate::ConfiguratorSpec>) {
+        todo!()
+    }
+}
