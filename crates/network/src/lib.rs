75d104053ac029c908b784c8f5882501a163de73
FL03 <jo3mccain@icloud.com>
2022-06-30 11:44:16 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- crates/network/src/lib.rs
+++ crates/network/src/lib.rs
@@ -1,8 +1,11 @@
 /*
     Appellation: acme-network
+    Context: library
     Creator: FL03 <jo3mccain@icloud.com>
-    Context:
     Description:
+        This crate was created in support of acme, an all-in-one blockchain toolkit and enables the
+        developer to implement a number of standard networking features for building optimized EVM
+        side-chains.
  */
 pub use crate::{
     behaviours::*,
