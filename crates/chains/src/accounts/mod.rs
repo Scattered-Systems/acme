580baf5d1d7664ee26e47ce5994b02d560eaeaa3
FL03 <jo3mccain@icloud.com>
2022-06-30 20:00:34 -0500

    Update

--- crates/chains/src/accounts/mod.rs
+++ crates/chains/src/accounts/mod.rs
@@ -5,6 +5,33 @@
    Description:
        ... Summary ...
 */
-pub use account::*;
 
-mod account;
+#[derive(Clone, Debug, Hash, PartialEq, serde::Deserialize, serde::Serialize)]
+pub struct Account {
+    pub address: String,
+    pub alias: String,
+    pub key: String,
+    pub secret: String,
+    pub token: String,
+}
+
+impl Account {
+    pub fn new(address: String, alias: String, key: String, secret: String, token: String) -> Self {
+        Self {
+            address,
+            alias,
+            key,
+            secret,
+            token,
+        }
+    }
+    pub fn from(address: &str, alias: &str, key: &str, secret: &str, token: &str) -> Self {
+        Self::new(
+            String::from(address),
+            String::from(alias),
+            String::from(key),
+            String::from(secret),
+            String::from(token),
+        )
+    }
+}
