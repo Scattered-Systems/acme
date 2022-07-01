5e8b737df345ab571d42bcf7107c23516567a130
FL03 <jo3mccain@icloud.com>
2022-07-01 14:23:58 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- crates/acme/src/lib.rs
+++ crates/acme/src/lib.rs
@@ -5,6 +5,10 @@
     Description:
         Acme is the designated application library, strapping a cli with seperate async p2p applications
 */
+#[doc(inline)]
+#[cfg(feature = "default")]
+pub use acme_sdk::*;
+
 pub use crate::{configurations::*, interfaces::*};
 
 mod configurations;
