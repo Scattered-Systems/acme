55974e782343ce02bdba25c7805c0152684cc1c6
FL03 <jo3mccain@icloud.com>
2022-07-12 12:54:28 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- lib/sdk/src/data/models/mod.rs
+++ lib/sdk/src/data/models/mod.rs
@@ -1,7 +1,10 @@
 /*
-    Appellation: mod
-    Context:
+    Appellation: models
+    Context: module
     Creator: FL03 <jo3mccain@icloud.com>
     Description:
         ... Summary ...
  */
+pub use accounts::*;
+
+mod accounts;
