9c350165ab1906ae317c1ed40a77b4bf2a2e5bfa
Joe McCain III <jo3mccain@icloud.com>
2022-12-06 02:16:13 +0000

    Update

--- minis/src/actors/n2n/mod.rs
+++
@@ -1,9 +0,0 @@
-/*
-    Appellation: n2n <module>
-    Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
-    Description:
-        ... Summary ...
-*/
-pub use self::agent::NodeToNode;
-
-pub(crate) mod agent;
