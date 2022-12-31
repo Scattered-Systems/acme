14eeedb48b1597b0c5487ad998f0dd6b8d2c8bca
Joe McCain III <jo3mccain@icloud.com>
2022-12-31 21:51:52 +0000

    Updates

--- crates/net/src/lib.rs
+++ crates/net/src/lib.rs
@@ -9,3 +9,20 @@ pub mod middleware;
 pub mod proxies;
 pub mod servers;
 pub mod signals;
+
+use scsys::prelude::{AsyncResult, Contextual};
+use servers::ServerSpec;
+
+#[async_trait::async_trait]
+pub trait WebBackend {
+    type Ctx: Contextual;
+    type Server: ServerSpec + Send + Sync;
+
+    async fn client(&self) -> axum::Router;
+    fn context(&self) -> Self::Ctx;
+    fn server(&self) -> Self::Server;
+    /// Quickstart the server with the outlined client
+    async fn serve(&self) -> AsyncResult {
+        self.server().serve(self.client().await).await
+    }
+}
