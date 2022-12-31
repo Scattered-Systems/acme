67025769edee392847e2befc38392d5916cf6315
Joe McCain III <jo3mccain@icloud.com>
2022-12-31 19:08:01 +0000

    Update

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -3,12 +3,11 @@
     Contrib: FL03 <jo3mccain@icloud.com>
     Description: ... Summary ...
 */
-pub use self::{backend::*, primitives::*, specs::*, utils::*};
+pub use self::{primitives::*, specs::*, utils::*};
 
 pub mod events;
 pub mod sessions;
 
-pub(crate) mod backend;
 pub(crate) mod primitives;
 pub(crate) mod utils;
 
--- crates/core/src/primitives.rs
+++ crates/core/src/primitives.rs
@@ -11,18 +11,21 @@ pub(crate) mod constants {
     ///
     pub const ME_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59";
     ///
-    pub const LOCALHOST: super::HostPieces = [127, 0, 0, 1];
+    pub const LOCALHOST: super::Host = [127, 0, 0, 1];
 }
 
 pub(crate) mod types {
-
     ///
-    pub type HostPieces = [u8; 4];
+    pub type ChannelPackStd<T> = (std::sync::mpsc::Sender<T>, std::sync::mpsc::Receiver<T>);
+    
+    ///
+    pub type Host = [u8; 4];
     ///
     pub type Port = u16;
     ///
-    pub type SocketAddrPieces = (HostPieces, Port);
-    ///
     pub type ServerAddr = std::net::SocketAddr;
-
+    ///
+    pub type SocketAddrPieces = (Host, Port);
+    ///
+    pub type TokioChannelPackMPSC<T> = (tokio::sync::mpsc::Sender<T>, tokio::sync::mpsc::Receiver<T>);
 }
