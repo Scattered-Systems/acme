1a62a2debda123d99b48666d6914ef659dcb4ea6
Joe McCain III <jo3mccain@icloud.com>
2023-01-01 14:06:20 +0000

    Update

--- core/src/handlers/handler.rs
+++ core/src/handlers/handler.rs
@@ -3,7 +3,19 @@
     Contrib: FL03 <jo3mccain@icloud.com>
     Description: ... summary ...
 */
+use serde::{Deserialize, Serialize};
 
-pub struct Handler {
-    
+
+pub type TransitionFunction<S, T> = dyn Fn(S) -> T;
+
+pub trait Transition<S> {
+    type Output;
+
+    fn data(&self) -> S;
+    fn transition(&self, dirac: &TransitionFunction<S, Self::Output>) -> Self::Output {
+        dirac(self.data())
+    }
 }
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, Hash, PartialEq, Serialize)]
+pub struct Handler;
